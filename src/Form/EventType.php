339c56130b191f1b23d808c818820ca7268a2e81
Marie Le Goué <marie.le_goue@centrale-marseille.fr>
2020-02-09 19:43:47 +0100

    Review 2 partie 3

---
+++ src/Form/EventType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('location')
+            ->add('start', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => false
+            ])
+            ->add('end', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
